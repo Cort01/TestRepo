2496f8312829dcaff7825fe75f88b9f67311f51b
Cortland Donahey <jamesd1976@msn.com>
2015-07-28 11:05:52 -0700

    version 2 commit

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -20,7 +20,7 @@ namespace WindowsFormsApplication1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello Robert!");
+            MessageBox.Show("Hello Robert Version 2!");
         }
     }
 }
